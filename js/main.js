67a58984e8428847bcad83e21a2d06cec5887eba
Qingxiao da <Frank@Hengs-MacBook-Pro.local>
2020-02-25 22:12:12 +1100

    extend 'this' scope for all function in 'onclick'

--- js/main.js
+++ js/main.js
@@ -3,15 +3,15 @@ boardDimension = 3;
 const cells = document.querySelectorAll('.cell');
 let switchToggler = true;
 
-const cellFlip = function () {
+const cellFlip = function (_this) {
     const div = document.createElement('div');
     if (switchToggler) {
         div.setAttribute('class', 'circle');
     } else {
         div.setAttribute('class', 'cross');
     }
-    this.appendChild(div);
-    console.log('clicking: ' + this.row + this.column);
+    _this.appendChild(div);
+    console.log('clicking: ' + _this.row + _this.column);
 };
 
 const circle = {
@@ -29,6 +29,7 @@ const cross = {
 };
 
 const checkRow = function (side) {
+    // circle.row[_this.row] += 1;
     for (let i = 0; i < Object.keys(side.row).length; i++) {
         if (side.row[i] === 3) {    // 3 could be changed to variables if needed
             console.log(`${side} win`);
@@ -50,39 +51,37 @@ const checkDiag = function (side) {
     }
 };
 
-const cellClicking = function () {
-    this.style.backgroundColor = 'rgb(209, 110, 110, 0.4)';
+const cellClicking = function (_this) {
+    _this.style.backgroundColor = 'rgb(209, 110, 110, 0.4)';
     if (switchToggler) {
-        circle.row[this.row] += 1;
-        circle.column[this.column] += 1;
-        if (this.row === this.column) {
+        circle.row[_this.row] += 1;
+        circle.column[_this.column] += 1;
+        if (_this.row === _this.column) {
             circle.posDiag += 1;
         };
-        if ((this.row + this.column) === 2) {
+        if ((_this.row + _this.column) === 2) {
             circle.negDiag += 1;
         }
         checkRow(circle);
         checkColumn(circle);
         checkDiag(circle);
     } else {
-        cross.row[this.row] += 1;
-        cross.column[this.column] += 1;
-        if (this.row === this.column) {
+        cross.row[_this.row] += 1;
+        cross.column[_this.column] += 1;
+        if (_this.row === _this.column) {
             cross.posDiag += 1;
         };
-        if ((this.row + this.column) === 2) {
+        if ((_this.row + _this.column) === 2) {
             cross.negDiag += 1;
         }
         checkRow(cross);
         checkColumn(cross);
         checkDiag(cross);
     };
-    console.log(this.row +'' +this.column);
-    
 
     // After click
     switchToggler = !switchToggler;
-    this.removeEventListener('click', cellClicking);
+    _this.removeEventListener('click', cellClicking);
 };
 
 // Start
@@ -93,8 +92,8 @@ for (let i = 0; i < cells.length; i++) {
 
     // Bind onclick event to each cell
     cells[i].addEventListener('click', function () {
-        let _this = this;
-        cellClicking();
+        cellClicking(this);
+        cellFlip(this);
     });
     // cells[i].addEventListener('click', cellFlip);
 };
