4c256c0ca972fe89296286985c2bc348580e84b8
Qingxiao da <Frank@Hengs-MacBook-Pro.local>
2020-02-26 13:47:44 +1100

    work perfectly in 3x3 table

--- js/main.js
+++ js/main.js
@@ -1,19 +1,3 @@
-const inputBoardDimensions = parseInt(document.querySelector('#board-dimension').value);
-boardDimension = 3;
-const cells = document.querySelectorAll('.cell');
-let switchToggler = true;
-
-const cellFlip = function (_this) {
-    const div = document.createElement('div');
-    if (switchToggler) {
-        div.setAttribute('class', 'circle');
-    } else {
-        div.setAttribute('class', 'cross');
-    }
-    _this.appendChild(div);
-    _this.removeEventListener('click', cellFlip);
-};
-
 const circle = {
     row: {0: 0, 1: 0, 2: 0},
     column: {0: 0, 1: 0, 2: 0},
@@ -28,11 +12,34 @@ const cross = {
     negDiag: 0
 };
 
+// const winConditionStorage = function (shape, inputBoardDimensions) {
+//     for (let i = 0; i < inputBoardDimensions; i++) {
+//         shape.row.i = 0;
+//         shape.column.i = 0;
+//         shape.posDiag = 0;
+
+//     }
+// };
+
+const cellFlip = function (_this,) {
+    if (!gameover) {
+        const div = document.createElement('div');
+        if (circleFirst) {
+            div.setAttribute('class', 'circle');
+        } else {
+            div.setAttribute('class', 'cross');
+        };
+        _this.appendChild(div);
+        _this.removeEventListener('click', cellFlip);
+    };
+};
+
 const checkRow = function (side, _this) {
     side.row[_this.row] += 1;
     for (let i = 0; i < Object.keys(side.row).length; i++) {
         if (side.row[i] === 3) {    // 3 could be changed to variables if needed
             console.log(`win`);
+            gameover = true;
         };
     };
 };
@@ -42,6 +49,7 @@ const checkColumn = function (side, _this) {
     for (let i = 0; i < Object.keys(side.column).length; i++) {
         if (side.column[i] === 3) {    // 3 could be changed to variables if needed
             console.log(`win`);
+            gameover = true;
         };
     };
 };
@@ -52,31 +60,60 @@ const checkDiag = function (side, _this) {
     };
     if ((_this.row + _this.column) === 2) {
         side.negDiag += 1;
-    }
-    if (side.posdiag === 3 || side.negDiag === 3) {
+    };
+    if (side.posDiag === 3 || side.negDiag === 3) {
         console.log('win'); 
-    }
+        gameover = true;
+    };
 };
 
 const cellClicking = function (_this) {
-    _this.style.backgroundColor = 'rgb(209, 110, 110, 0.4)';
-    if (switchToggler) {
-        checkRow(circle, _this);
-        checkColumn(circle, _this);
-        checkDiag(circle, _this);
-    } else {
-        checkRow(cross, _this);
-        checkColumn(cross, _this);
-        checkDiag(cross, _this);
+    if (!gameover) {
+        cellFlip(_this);
+        count += 1;
+        // _this.style.backgroundColor = 'rgb(209, 110, 110, 0.4)';
+        if (circleFirst) {
+            checkRow(circle, _this);
+            checkColumn(circle, _this);
+            checkDiag(circle, _this);
+        } else {
+            checkRow(cross, _this);
+            checkColumn(cross, _this);
+            checkDiag(cross, _this);
+        };
+        if (count === 9) {
+            console.log('draw'); 
+            gameover = true;
+        };
+        // After click
+        circleFirst = !circleFirst;
+        _this.removeEventListener('click', cellClicking);
     };
-    // After click
-    switchToggler = !switchToggler;
-    _this.removeEventListener('click', cellClicking);
-    console.log('haha');
-    
 };
 
+const createCell = function (inputBoardDimensions) {
+    root.style.setProperty('--board-dimension', inputBoardDimensions);
+    const wrap = document.querySelector('.board');
+    for (let i = 0; i < inputBoardDimensions * inputBoardDimensions; i++) {
+        wrap.innerHTML += '<div class="cell"></div>'
+    };
+};
+
+// Variables declaration
+const inputBoardDimensions = parseInt(document.querySelector('#board-dimension').value);
+boardDimension = 3;
+let circleFirst = true;
+let gameover = false;
+let count = 0;
+let root = document.documentElement;
+
+
+
+createCell(3);
+const cells = document.querySelectorAll('.cell');
+
 // Start
+
 for (let i = 0; i < cells.length; i++) {
     // Define cell's own row and column no.
     cells[i].row = Math.floor(i / 3);
@@ -85,7 +122,7 @@ for (let i = 0; i < cells.length; i++) {
     // Add onclick event for each cell and unbind after clicked
     cells[i].addEventListener('click', function cellOnClick () {
         cellClicking(this);
-        cellFlip(this);
         this.removeEventListener('click', cellOnClick);
     });
 };
+
